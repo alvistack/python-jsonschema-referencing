287787aa0de7a5c14cfb70c1c55049fd3636eec2
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2024-01-29 11:20:48 +0800

    alvistack/v0.33.0
        git clean -xdf
        git submodule sync
        git submodule update --init
        git submodule foreach git checkout HEAD -- .
        tar zcvf ../python-referencing_0.33.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-referencing.spec ../python-referencing_0.33.0-1.spec
        cp ../python*-referencing*0.33.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/python-jsonschema-referencing-0.33.0/
        rm -rf ../python*-referencing*0.33.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='referencing',
+    version='0.33.0',
+    description='JSON Referencing + Python',
+    author='Julian Berman',
+    author_email='Julian+referencing@GrayVines.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: File Formats :: JSON',
+        'Topic :: File Formats :: JSON :: JSON Schema',
+    ],
+    install_requires=[
+        'attrs>=22.2.0',
+        'rpds-py>=0.7.0',
+    ],
+    packages=[
+        'referencing',
+    ],
+)
